4001f836e27a7068e0aeca1acea9081692f78d5a
Sir-Ob-00 <owusuboat05@gmail.com>
2025-01-08 22:42:36 -0800

    first commit

---
+++ js/index_array.js
@@ -0,0 +1,24 @@
+export const productImages = [
+    {
+        id: 0,
+        image: '',
+        name: 'YAMAHA PSR SX900',
+        description: 'The top-of-the-line in the PSR-SX Series, the PSR-SX900 delivers the best and most available in its class of Arranger Workstations today',
+        price: '',
+    },
+    {
+        id: 1,
+        image: '',
+        name: 'YAMAHA GENOS 2',
+        description:'',
+        price: '',
+    },
+    {
+        id: 2,
+        image: '',
+        name: 'YAMAHA PSR SX900',
+        description:'',
+        price: '',
+    }
+
+]
