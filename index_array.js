c3f2d0afb42c2d25595924cf55fcc593a0d79536
Sir-Ob-00 <owusuboat05@gmail.com>
2025-01-17 14:19:31 -0800

    functionality_updated

---
+++ index_array.js
@@ -0,0 +1,45 @@
+export const products = [
+    {
+        id: 0,
+        image: '../images/Yamaha-keyboard-E473.webp',
+        name: 'YAMAHA PSRSE473',
+        description: 'The touch-sensitive keyboard and powerful sound engine of the PSR-E473 bring together superb sound and great feel in an all-new portable instrument.',
+        price: 'GH₵6,500',
+    },
+    {
+        id: 1,
+        image: '../images/sx900 full.jpg',
+        name: 'YAMAHA PSRSX900',
+        description:'The top-of-the-line in the PSR-SX Series, the PSR-SX900 delivers the best and most available in its class of Arranger Workstations today.',
+        price: 'GH₵25,000',
+    },
+    {
+        id: 2,
+        image: '../images/yamaha genos.jpg',
+        name: 'YAMAHA GENOS',
+        description:'For the music enthusiast and solo performer, we present the new GENOS 2: a game changer in Arranger Workstations.',
+        price: 'GH₵30,000',
+    },
+    {
+        id: 0,
+        image: '../images/genos2 f.jpg',
+        name: 'YAMAHA GENOS 2',
+        description: 'The top-of-the-line in the PSR-SX Series, the GENOS 2 delivers the best and most available in its class of Arranger Workstations today',
+        price: 'GH₵45,000',
+    },
+    {
+        id: 1,
+        image: '../images/yamaha modx 8.jpg',
+        name: 'YAMAHA MODX 8',
+        description:'For the music enthusiast and solo performer, we present the new MODX 8: a game changer in Arranger Workstations.',
+        price: 'GH₵65,000',
+    },
+    {
+        id: 2,
+        image: '../images/yamaha montage 8.jpg',
+        name: 'YAMAHA MONTAGE',
+        description:'The touch-sensitive keyboard and powerful sound engine of the MONTAGE 8 bring together superb sound and great feel in an all-new portable instrument.',
+        price: 'GH₵80,000',
+    }
+
+]
